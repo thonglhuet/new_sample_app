646647ed0880482c751e7963c7a28b374281fd44
Thong <thonglh.uet@gmail.com>
2017-03-01 13:49:58 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+    not_found if @user.nil?
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".welcome"
+      flash[:f] = "52313131"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
   end
 end
