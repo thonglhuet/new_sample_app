003fe1c1c7099f0c59ea953c0164637dc256be1e
Thong <thonglh.uet@gmail.com>
2017-03-03 16:05:54 +0700

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,12 +8,11 @@ module SessionsHelper
       @current_user ||= User.find_by id: user_id
     elsif user_id = cookies.signed[:user_id]
       user = User.find_by id: user_id
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
-
   end
 
   def current_user? user
@@ -28,11 +27,6 @@ module SessionsHelper
 
   def logged_in?
     current_user.present?
-  end
-
-  def log_out
-    session.delete :user_id
-    @current_user = nil
   end
 
   def forget user
